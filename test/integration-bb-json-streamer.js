4d2b04b81c45188651895a6ed59fc492a9a223fc
Lam Chan <lchan@xogrp.com>
2017-02-26 23:38:21 -0600

    WIP shuffling npm link to travis file

--- test/integration-bb-json-streamer.js
+++ test/integration-bb-json-streamer.js
@@ -21,20 +21,17 @@ describe('bb-json-streamer', () => {
     it('should pipe a single object.json to stdout', (done) => {
 
         const result = Shell.exec('cat ./test/mocks/object.json | bb-json-streamer');
-        //const result = Shell.exec('echo $PWD');
-
-
         const source = JSON.stringify(require('./mocks/object.json'));
+
         expect(result.stdout).to.be.equal(source + '\n');
         done();
     });
 
     it('should pipe multiple object.json to stdoout', (done) => {
 
-        //const result = Shell.exec('cat test/mocks/object.json test/mocks/object.json test/mocks/object.json | bb-json-streamer');
-        const result = Shell.exec('ls -l test/mocks');
-
+        const result = Shell.exec('cat test/mocks/object.json test/mocks/object.json test/mocks/object.json | bb-json-streamer');
         const source = JSON.stringify(require('./mocks/object.json'));
+
         expect(result.stdout).to.be.equal(source + '\n' + source + '\n' + source + '\n');
         done();
     });
