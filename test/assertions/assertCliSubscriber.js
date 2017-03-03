b2f615f2170762b05ba455edea9f565f0c5266fc
Lam Chan <lchan@xogrp.com>
2017-03-03 17:43:01 -0600

    testing linux incompatiblity

--- test/assertions/assertCliSubscriber.js
+++ test/assertions/assertCliSubscriber.js
@@ -20,7 +20,8 @@ const assertCliSubscriber = (bunnyBus, queueName, iterations, callback, duration
             // wc - counts the lines of output
             // xargs - strips out the number only
             // tr - removes unwanted character
-            Exec(`bunnyBus -S -c ${configurationPath} -d ${duration} | wc -l | xargs | tr -d "\n"`, (err, stdout) => {
+            // Exec(`bunnyBus -S -c ${configurationPath} -d ${duration} | wc -l | xargs | tr -d "\n"`, (err, stdout) => {
+                Exec(`bunnyBus -S -c ${configurationPath} -d ${duration}`, (err, stdout) => {
                 console.log(stdout);
                 expect(err).to.be.null();
                 // expect(stdout).to.be.equal(iterations.toString());
