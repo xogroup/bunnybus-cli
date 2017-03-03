ed7c61f844c3c1345d2b31957e7e2d0c67461644
Lam Chan <lchan@xogrp.com>
2017-03-03 17:20:46 -0600

    adjusting timing again

--- test/integration-bunnybus.js
+++ test/integration-bunnybus.js
@@ -135,17 +135,17 @@ describe('bunnybus', () => {
 
         it('should subscribe 1 object', (done) => {
 
-            Assertions.assertCliSubscriber(bunnyBus, queueName, 1, done, 100);
+            Assertions.assertCliSubscriber(bunnyBus, queueName, 1, done, 200);
         });
 
         it('should subscribe 100 object', (done) => {
 
-            Assertions.assertCliSubscriber(bunnyBus, queueName, 100, done, 600);
+            Assertions.assertCliSubscriber(bunnyBus, queueName, 100, done, 800);
         });
 
         it('should subscribe 1000 object', (done) => {
 
-            Assertions.assertCliSubscriber(bunnyBus, queueName, 1000, done, 1200);
+            Assertions.assertCliSubscriber(bunnyBus, queueName, 1000, done, 1400);
         });
     });
 });
